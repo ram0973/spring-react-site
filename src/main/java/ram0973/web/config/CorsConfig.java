690628778fc47fbc311eb2d8ef4144f39aa9dce3
ram0973 <ram0973@gmail.com>
2024-07-22 16:54:22 +0300

    next features

--- src/main/java/ram0973/web/config/CorsConfig.java
+++ src/main/java/ram0973/web/config/CorsConfig.java
@@ -16,35 +16,47 @@ import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 public class CorsConfig {
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(
-//            List.of("http://localhost:5173", "http://localhost:5173/", "http://localhost")
-//        ); // React
-
-        configuration.setAllowedMethods(Arrays.asList("GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"));
-
-        configuration.applyPermitDefaultValues();
-//        configuration.setAllowedHeaders(List.of(
-//            "Origin", //OPTIONS
-//            "Access-Control-Allow-Headers",
-//            "Access-Control-Allow-Origin",
-//            "Access-Control-Request-Method", //OPTIONS
-//            "Access-Control-Request-Headers", //OPTIONS optionally
-//            "Cache-Control",
-//            "Content-Type",
-//            "Authorization",
-//            "X-Requested-With",
-//            "Accept"
-//        ));
-        configuration.setAllowCredentials(true);
-        configuration.addAllowedOrigin("http://localhost:5173");
-        configuration.addAllowedOrigin("http://127.0.0.1:5173");
-        configuration.addAllowedOrigin("http://127.0.0.1:8080");
-        configuration.addAllowedOrigin("http://localhost:8080");
-
+    public CorsConfigurationSource corsConfigurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("http://127.0.0.1:4200");
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
         return source;
     }
+
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+////        configuration.setAllowedOrigins(
+////            List.of("http://localhost:5173", "http://localhost:5173/", "http://localhost")
+////        ); // React
+//
+//        configuration.setAllowedMethods(Arrays.asList("GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"));
+//
+//        configuration.applyPermitDefaultValues();
+////        configuration.setAllowedHeaders(List.of(
+////            "Origin", //OPTIONS
+////            "Access-Control-Allow-Headers",
+////            "Access-Control-Allow-Origin",
+////            "Access-Control-Request-Method", //OPTIONS
+////            "Access-Control-Request-Headers", //OPTIONS optionally
+////            "Cache-Control",
+////            "Content-Type",
+////            "Authorization",
+////            "X-Requested-With",
+////            "Accept"
+////        ));
+//        configuration.setAllowCredentials(true);
+//        configuration.addAllowedOrigin("http://localhost:5173");
+//        configuration.addAllowedOrigin("http://127.0.0.1:5173");
+//        configuration.addAllowedOrigin("http://127.0.0.1:8080");
+//        configuration.addAllowedOrigin("http://localhost:8080");
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 }
